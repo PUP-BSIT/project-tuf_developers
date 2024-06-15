ef944c08951e1294ff361d874de3bd6128f84c26
nogadas-vonryan <vonnogadas251@gmail.com>
2024-06-15 16:00:44 +0800

    add constants to get html elements

---
+++ Iraya/scripts/edit_journal.js
@@ -0,0 +1,3 @@
+const journalId = document.querySelector('#journal_id');
+const journalTitle = document.querySelector('#journal_title');
+const journalContent = document.querySelector('#journal_content');
