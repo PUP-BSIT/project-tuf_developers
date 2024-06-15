31f8ac4acf0d2f8276503bc8917f6c0466f10e3b
nogadas-vonryan <vonnogadas251@gmail.com>
2024-06-15 16:04:09 +0800

    add function to display journals in the journal manager page

--- Iraya/scripts/journal_manager.js
+++ Iraya/scripts/journal_manager.js
@@ -8,3 +8,37 @@ fetch(endpoint)
 		journalData = data;
         displayJournals();
 	});
+    
+function displayJournals() {
+    const FIELD = {
+        JOURNAL_ID: 0,
+        USER_ID: 1,
+        TITLE: 2,
+        CONTENT: 3,
+        STICKER: 4
+    };
+
+    for(item of journalData) {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>
+                ${item[FIELD.TITLE]}
+            </td>
+            <td>
+                ${item[FIELD.CONTENT]}
+            </td>`;
+        
+        const {editButton, deleteButton} = createActionButtons(
+            item[FIELD.JOURNAL_ID]);
+        
+        const editColumn = document.createElement('td');
+        editColumn.append(editButton);
+        const deleteColumn = document.createElement('td');
+        deleteColumn.append(deleteButton);
+
+        row.append(editColumn);
+        row.append(deleteColumn);
+        
+        journals.append(row);
+    }
+}
