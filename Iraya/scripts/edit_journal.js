49873096bef6bc3b0eeb5c92540f60f8be8f2809
nogadas-vonryan <vonnogadas251@gmail.com>
2024-06-15 16:01:11 +0800

    add function to edit journals in js via ajax

--- Iraya/scripts/edit_journal.js
+++ Iraya/scripts/edit_journal.js
@@ -1,3 +1,20 @@
 const journalId = document.querySelector('#journal_id');
 const journalTitle = document.querySelector('#journal_title');
 const journalContent = document.querySelector('#journal_content');
+
+async function editJournal() {
+    const options = {
+        method: 'PATCH',
+        headers: {
+            "Content-type": "application/x-www-form-urlencoded",
+        },
+        body: `journal_id=${journalId.value}&\
+            journal_title=${journalTitle.value}&\
+            journal_content=${journalContent.value}`
+    };
+
+    const response = await fetch('./api.php', options);
+    const data = await response.text();
+
+    window.location.assign('./journal_manager.php');
+}
