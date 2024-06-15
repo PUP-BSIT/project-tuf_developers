e7e735756afd180a99992dee8585599158fa5d0e
nogadas-vonryan <vonnogadas251@gmail.com>
2024-06-15 16:03:09 +0800

    add fetch call to get data from endpoint

--- Iraya/scripts/journal_manager.js
+++ Iraya/scripts/journal_manager.js
@@ -1,3 +1,10 @@
 const journals = document.querySelector("#journals");
 const endpoint = './api.php';
 let journalData;
+
+fetch(endpoint)
+	.then((result) => result.json())
+	.then((data) => {
+		journalData = data;
+        displayJournals();
+	});
