ff16bb2e9887e1aed883bd76531f2a1354fdd32f
nogadas-vonryan <vonnogadas251@gmail.com>
2024-06-15 15:44:11 +0800

    add get request method to send json

--- Iraya/api.php
+++ Iraya/api.php
@@ -5,3 +5,13 @@ require_once './user.php';
 if (!isUserLoggedIn()) {
     die('{ "error": "Invalid Access!" }');
 }
+
+$userId = $_SESSION['user_id'];
+
+if($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $journals = getAllJournals($userId);
+    $result = $journals->fetch_all();
+    $json = json_encode($result);
+
+    echo $json;
+}
