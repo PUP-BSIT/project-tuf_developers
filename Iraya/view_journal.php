59b15f4994f8b875167e97c4958573535041504e
cahigan-marklouie <markcahigan54@gmail.com>
2024-06-22 13:31:57 +0800

    add css link in view_journal.php

--- Iraya/view_journal.php
+++ Iraya/view_journal.php
@@ -15,6 +15,7 @@ $row = $result->fetch_assoc();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>View Journal</title>
+    <link rel="stylesheet" href="./asset/stylesheets/view_journal.css" />
 </head>
 
 <body>
