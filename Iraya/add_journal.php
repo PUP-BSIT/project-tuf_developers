86a6708e289b198d51833ea67bc3cc9f2d609393
mauroche <rochemaui165@gmail.com>
2024-06-28 20:02:19 +0800

    add images

--- Iraya/add_journal.php
+++ Iraya/add_journal.php
@@ -1,11 +1,14 @@
 <?php
+    /*
     session_start();
     require_once 'user.php';
 
     if(!isUserLoggedIn()) {
         header('Location:login.php');
     }
+    */
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
