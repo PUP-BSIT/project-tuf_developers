b553e67f1267860768ce01afb442dec992ba6a25
Andreadonatos <dreadonatos@gmail.com>
2024-06-29 04:26:32 +0800

    add button to journal_manager.php

--- Iraya/journal_manager.php
+++ Iraya/journal_manager.php
@@ -39,6 +39,9 @@ if (!isUserLoggedIn()) {
   <div class="main-content">
     <a href="./add_journal.php" class="add-journal">Add Journal</a>
     <table id="journals">
+    <button type="button" onclick="viewJournal()">View</button>
+    <button type="button" onclick="editJournal()">Edit</button>
+    <button type="button" onclick="deleteJournal()">Delete</button>
       <tr>
           <th>Title</th>
           <th>Content</th>
