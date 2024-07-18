4a69c2501c859c4c1fd35510131e9051c486f170
Andreadonatos <dreadonatos@gmail.com>
2024-07-18 19:35:03 +0800

    add constants for registration form elements

---
+++ Iraya/scripts/register.js
@@ -0,0 +1,10 @@
+const form = document.querySelector('form');
+const username = document.querySelector('#username');
+const password = document.querySelector('#password');
+const confirm = document.querySelector('#confirm');
+
+const usernameMessage = document.querySelector('#username_message');
+const passwordMessage = document.querySelector('#password_message');
+const confirmMessage = document.querySelector('#confirm_message');
+
+let isPasswordValid = false;
