4965351c05afee1fd25e7d1f5fb8d3ef4c02ccc1
nogadas-vonryan <vonnogadas251@gmail.com>
2024-07-19 01:09:44 +0800

    update search and display function for selected journals

--- Iraya/scripts/journal_manager.js
+++ Iraya/scripts/journal_manager.js
@@ -1,11 +1,11 @@
 const journals = document.querySelector("#journals");
-const search = document.querySelector('#search');
+const search = document.querySelector("#search");
 
-const clearButton = document.querySelector('#clear_button');
-const editButton = document.querySelector('#edit_button');
-const deleteButton = document.querySelector('#delete_button');
+const clearButton = document.querySelector("#clear_button");
+const editButton = document.querySelector("#edit_button");
+const deleteButton = document.querySelector("#delete_button");
 
-const endpoint = './api.php';
+const endpoint = "./api.php";
 let journalData;
 let searchData;
 let selectedJournals = [];
@@ -14,82 +14,130 @@ fetch(endpoint)
 	.then((result) => result.json())
 	.then((data) => {
 		journalData = data;
-        displayJournals();
+		displayJournals();
 	});
-    
+
 function displayJournals() {
-    const FIELD = {
-        JOURNAL_ID: 0,
-        USER_ID: 2,
-        TITLE: 3,
-        CONTENT: 4
-    };
+	journals.innerHTML = "";
 
-    for(item of journalData) {
-        const row = document.createElement('tr');
-        row.innerHTML = `
-            <td>
-                ${item[FIELD.TITLE]}
-            </td>
-            <td>
-                ${item[FIELD.CONTENT]}
-            </td>`;
-        
-        const {viewButton, editButton, deleteButton} = createActionButtons(
-            item[FIELD.JOURNAL_ID]);
-        
-        const viewColumn = document.createElement('td');
-        viewColumn.append(viewButton);
-        const editColumn = document.createElement('td');
-        editColumn.append(editButton);
-        const deleteColumn = document.createElement('td');
-        deleteColumn.append(deleteButton);
+	const FIELD = {
+		JOURNAL_ID: 0,
+		USER_ID: 2,
+		TITLE: 3,
+		CONTENT: 4,
+		DATE_CREATED: 5,
+		DATE_UPDATED: 6,
+	};
 
-        row.append(viewColumn);
-        row.append(editColumn);
-        row.append(deleteColumn);
-        
-        journals.append(row);
-    }
-}
+	for (item of searchData) {
+		const row = document.createElement("button");
+		row.id = item[FIELD.JOURNAL_ID];
+		row.classList.add("card-list");
 
-function createActionButtons(journalId) {
-    const viewButton = document.createElement('button');
-    const editButton = document.createElement('button');
-    const deleteButton = document.createElement('button'); 
+		const date = new Date(item[FIELD.DATE_UPDATED]).toDateString();
 
-    viewButton.textContent = 'View';
-    editButton.textContent = 'Edit';
-    deleteButton.textContent = 'Delete';
+		row.innerHTML = `
+                <div class="pad-1 strong"> ${item[FIELD.TITLE]} </div>
+                <div class="flex center pad-1 width-100 text-left">
+                    <div>${item[FIELD.CONTENT]}</div>
+                </div>
+                <div>${date}</div>`;
 
-    viewButton.addEventListener('click', () => viewJournal(journalId));
-    editButton.addEventListener('click', () => editJournal(journalId));
-    deleteButton.addEventListener('click', () => deleteJournal(journalId));
-
-    return { viewButton, editButton, deleteButton };
-}
-
-function viewJournal(journalId) {
-    const view_endpoint = 'view_journal.php';
-    window.location.replace(`./${view_endpoint}?journal_id=${journalId}`);
+		row.onclick = selectJournal.bind(null, item[FIELD.JOURNAL_ID]);
+		journals.append(row);
+	}
 }
 
 function editJournal(journalId) {
-    const edit_endpoint = 'edit_journal.php';
-    window.location.replace(`./${edit_endpoint}?journal_id=${journalId}`);
+	const edit_endpoint = "edit_journal.php";
+	window.location.replace(`./${edit_endpoint}?journal_id=${journalId}`);
 }
 
-async function deleteJournal(journalId) {
-    const options = {
-        method: 'DELETE',
-        headers: {
-            "Content-type": "application/x-www-form-urlencoded",
-        },
-        body: `journal_id=${journalId}`
-    };
+function searchJournals() {
+	searchData = journalData.filter((item) => {
+		return (
+			item[3].includes(search.value) ||
+			item[4].includes(search.value)
+		);
+	});
 
-    const response = await fetch(endpoint, options);
-    const data = await response.text();
+	selectedJournals = [];
+	sortDate();
+}
 
-    window.location.replace('./journal_manager.php');
+function selectJournal(journalId, event) {
+	const button = event.currentTarget;
+
+	if (button.classList.contains("card-selected")) {
+		selectedJournals = selectedJournals.filter(
+			(item) => item.id !== button.id
+		);
+		button.classList.remove("card-selected");
+
+		if (selectedJournals.length == 1)
+			editButton.classList.remove("none");
+		if (selectedJournals.length == 0) {
+			clearButton.classList.add("none");
+			editButton.classList.add("none");
+			deleteButton.classList.add("none");
+		}
+		return;
+	}
+
+	selectedJournals.push(button);
+
+	button.classList.add("card-selected");
+	clearButton.classList.remove("none");
+	editButton.classList.remove("none");
+	deleteButton.classList.remove("none");
+
+	if (selectedJournals.length > 1) editButton.classList.add("none");
+
+	const edit_endpoint = "edit_journal.php";
+	editButton.onclick = () => {
+		location.replace(`./${edit_endpoint}?journal_id=${journalId}`);
+	};
+}
+
+function clearAllSelection() {
+	for (const button of selectedJournals) {
+		button.classList.remove("card-selected");
+	}
+
+	selectedJournals = [];
+	clearButton.classList.add("none");
+	editButton.classList.add("none");
+	deleteButton.classList.add("none");
+}
+
+async function deleteAllSelection() {
+	for (const item of selectedJournals) {
+		await fetch(endpoint, {
+			method: "DELETE",
+			headers: {
+				"Content-type": "application/x-www-form-urlencoded",
+			},
+			body: `journal_id=${item.id}`,
+		});
+	}
+
+	window.location.reload();
+}
+
+function sortDate() {
+	const sortInput = document.querySelector("#sort_journals");
+	const SORTDATE = {
+		ASCENDING: "date_ascending",
+		DESCENDING: "date_descending",
+	};
+
+	searchData.sort((a, b) => {
+		if (sortInput.value == SORTDATE.ASCENDING) {
+			return new Date(a[6]) - new Date(b[6]);
+		} else {
+			return new Date(b[6]) - new Date(a[6]);
+		}
+	});
+
+	displayJournals();
 }
