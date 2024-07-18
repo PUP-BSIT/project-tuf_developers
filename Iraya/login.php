2b44f638c95f3954abee2b98187bc3c7da595b85
nogadas-vonryan <vonnogadas251@gmail.com>
2024-07-19 02:01:18 +0800

    update login styling and alert

--- Iraya/login.php
+++ Iraya/login.php
@@ -2,6 +2,8 @@
     require_once 'user.php';
     session_start();
 
+    $message = $_GET['message'] ?? '';
+
     if(isUserLoggedIn()){
         header('Location:journal_manager.php');
     }
@@ -19,11 +21,12 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="./assets/stylesheets/login.css" />
+    <link rel="stylesheet" href="./assets/stylesheets/main.css" />
+    <link id="theme" rel="stylesheet" href="./assets/stylesheets/light.css">    
     <title>Login</title>
   </head>
   <body>
-    <header>
+    <header class="down-3">
       <div class="logo-container">
         <img src="./assets/images/logo.png" alt="Iraya Logo" class="logo" />
         <div class="logo-text">
@@ -40,28 +43,39 @@
         </ul>
       </nav>
     </header>
-    <div>
-      <form method="POST">
+    <div class="flex center down-6">
+      <form method="POST" class="flex column width-50">
         <div class="title">
           <h1>Login</h1>
         </div>
-        <div>
+        <div class="flex column">
           <label for="username">Username</label>
           <input type="text" name="username" required />
         </div>
-        <div>
+        <div class="flex column">
           <label for="password">Password</label>
           <input type="password" name="password" required />
         </div>
+        <div id="message"> <?= $message ?? '' ?> </div>
         <button type="submit">Login</button>
+        <div class="text-center">
+          <a href="./forgot_password.php">Forgot Password?</a>
+        </div>
+        <hr class="width-100">
+        <div class="flex center">
+          <a href="./register.php" class="btn">Create New Account</a>
+        </div>
       </form>
     </div>
     <section class="footer">
-      <div class="copyright">
-      <p>
-        © 2024 by Iraya. All rights reserved.
-      </p>
+      <div class="text-center">
+      <p> © 2024 by Iraya. All rights reserved. </p>
       </div>
     </section>
+    <button onclick="changeTheme('./assets/stylesheets')" 
+      class="theme-button">Change Theme</button>
+    <script src="./scripts/login.js"></script>
+    <script src="./scripts/theme.js" 
+      onload="loadTheme('./assets/stylesheets')"></script>
   </body>
 </html>
