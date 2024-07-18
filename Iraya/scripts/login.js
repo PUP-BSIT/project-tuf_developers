141b2a9591b22536794d9b4133b96707ce56262c
cahigan-marklouie <markcahigan54@gmail.com>
2024-07-18 20:34:44 +0800

    add null check for alert message

--- Iraya/scripts/login.js
+++ Iraya/scripts/login.js
@@ -8,3 +8,12 @@ const messageText = document.querySelector('#message');
 const urlParams = new URLSearchParams(window.location.search);
 const message = urlParams.get('message');
 let type = urlParams.get('type');
+
+if(!type) { 
+    type = STATUS_TYPE.WARN;
+}
+
+if(message) {
+    messageText.classList.add(type);
+    alert(message);
+}
