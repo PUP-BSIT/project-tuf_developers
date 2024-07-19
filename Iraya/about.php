cf9d2790e3b5951e09525840b1668bbbaebb01e5
mauroche <rochemaui165@gmail.com>
2024-07-19 23:49:21 +0800

    change contents to about page

--- Iraya/about.php
+++ Iraya/about.php
@@ -30,9 +30,12 @@
   <main class="pad-3">
     <div class="heading text-center down-5">
       <h1 class="header-about f-3">About Iraya</h1>
-      <p>Iraya is an e-journal tool where you can list, organize,
-        and manage your digital journals, making it easy to track and
-        analyze your entries over time.
+      <p>Iraya is an e-journal tool where you can list and track your mood, 
+        manage your to-do list, and easily track and analyze your entries
+        over time. Our vision is to create a digital space where users can
+        document and preserve their thoughts and memories. And 
+        Our mission is to simplify and enhance the user journaling
+        experience, making it a delightful journey through nostalgia.
       </p>
     </div>
     <div class="about-container">
@@ -41,66 +44,65 @@
           <img src="./assets/images/vision_img.png" alt="Picture">
         </div>
         <div class="about-content">
-          <h2>Capture moments with Iraya</h2>
-          <p> Iraya, your modern solution to journaling
-            that blends innovation with inspiration. At Iraya, we believe in
-            the power of capturing life moments, reflections, and aspirations
-            in a way that is both secure and creatively fulfilling.
-            Our mission is to simplify and enhance your journaling experience,
-            making it a delightful journey through nostalgia.
+          <h2>Story Behind Iraya</h2>
+          <p> A song by Just Hush called "Iraya." The song beautifully 
+              captures the essence of nostalgia, the passage of time, 
+              and the enduring beauty or resilience of a person or place. 
+              We decided to name our project "Iraya" to pay  deference to the
+              song that moved us so deeply. We envisioned an e-journal tool 
+              that would allow people to document their thoughts, dreams, 
+              and memories, creating a personal space where they could
+              revisit and reflect on their journey.  We wanted to create a 
+              digital space where users could write, store, and organize 
+              their memories seamlessly. The journey of developing Iraya was
+              filled with dedication and creativity. We focused on creating a 
+              user-friendly interface that captures the essence of our 
+              inspiration.  We wanted users to feel a connection to their
+              past, present, and future through the entries they create in 
+              Iraya.
           </p>
-          <h2>Our Vision</h2>
-          <p> In today's fast-paced world, we understand the importance
-            of preserving memories while adapting to ever-evolving digital
-            landscapes. Iraya aims to bridge the gap between traditional
-            journaling and modern technology, offering a user-centric platform
-            that caters to your personal and creative needs.</p>
+        </div>
+      </section>
+    </div>
+    <section class="team">
+        <div class="center">
+          <h1>Developers of Iraya</h1>
+        </div>
+        <div class="team-container flex text-center">
+          <div class="box">
+            <img src="./assets/images/Añonuevo_DIT.jpg"
+               alt="Jheferson's Picture" class="about-image">
+            <h3>Jheferson Añonuevo</h3>
+            <h5>Project Manager</h5>
+            <div class="icons"></div>
           </div>
-        </section>
-        <section class="team">
-          <div class="center">
-            <h1>TUF Developers</h1>
+          <div class="box">
+            <img src="./assets/images/Roche_DIT.jpg"
+               alt="Maui's Picture" class="about-image">
+            <h3>Maui Jane Roche</h3>
+            <h5>UI/UX Designer</h5>
+            <div class="icons"></div>
           </div>
-          <div class="team-container flex text-center">
-            <div class="box">
-              <img src="./assets/images/Añonuevo_DIT.jpg" 
-                alt="Jheferson's Picture" class="width-75 about-image">
-                <h3>Jheferson Añonuevo</h3>
-                <h5>Project Manager</h5>
-            <div class="icons">
-            </div>
-          </div>
-          <div>
-            <img src="./assets/images/Roche_DIT.jpg" alt="Maui's Picture" 
-              class="width-75 about-image">
-              <h3>Maui Jane Roche</h3>
-              <h5>UI/UX Designer</h5>
-              <div class="icons">
-          </div>
-          </div>
-          <div>
-            <img src="./assets/images/Nogadas_DIT.jpg" alt="Von's Picture" 
-              class="width-75 about-image">
+          <div class="box">
+            <img src="./assets/images/Nogadas_DIT.jpg"
+               alt="Von's Picture" class="about-image">
             <h3>Von Ryan Nogadas</h3>
             <h5>Lead Developer</h5>
-            <div class="icons">
+            <div class="icons"></div>
           </div>
-          </div>
-          <div>
-            <img src="./assets/images/Donatos_DIT.jpg" alt="Andrea's Picture" 
-              class="width-75 about-image">
+          <div class="box">
+            <img src="./assets/images/Donatos_DIT.jpg"
+               alt="Andrea's Picture" class="about-image">
             <h3>Andrea Donatos</h3>
             <h5>UI/UX Designer</h5>
-            <div class="icons">
-            </div>
+            <div class="icons"></div>
           </div>
-          <div>
-            <img src="./assets/images/Cahigan_DIT.jpg" alt="Mark's Picture" 
-              class="width-75 about-image">
+          <div class="box">
+            <img src="./assets/images/Cahigan_DIT.jpg"
+               alt="Mark's Picture" class="about-image">
             <h3>Mark Cahigan</h3>
             <h5>Tester</h5>
-            <div class="icons">
-            </div>
+            <div class="icons"></div>
           </div>
         </div>
       </section>
