29e42b417052bfcb8b37b38bb11eac17ec917c88
nogadas-vonryan <vonnogadas251@gmail.com>
2024-07-19 15:39:21 +0800

    add mood and themes

--- Iraya/about.php
+++ Iraya/about.php
@@ -119,7 +119,8 @@
       <p> © 2024 by Iraya. All rights reserved. </p>
     </div>
   </section>
-  <button onclick="changeTheme()" class="theme-button">Change Theme</button>
+  <button onclick="changeTheme('./assets/stylesheets')" 
+    class="theme-button">Change Theme</button>
   <script src="./scripts/theme.js" 
       onload="loadTheme('./assets/stylesheets')"></script>
 </body>
